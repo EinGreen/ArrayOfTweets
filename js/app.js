eb17e391c67d9ed1302d16af741a050c928e7bdd
Ein_Green <mbananola2002@gmail.com>
2021-03-19 23:57:04 -0700

    I just noticed that I forgot to save, so there was no js connected to index

---
+++ js/app.js
@@ -0,0 +1,14 @@
+// made an array of "tweets", some of them I made, most of them I stole, all of them are terrible
+var Tweets = [
+    "The Earth is Flat",
+    "News 101: Karens start taking over Edmonton",
+    "Florida Man arrested for fighting Aligator again!",
+    "I have no Idea what I'm doing",
+    "This is a Tweet",
+    "'Instagram is better', teen says",
+    "Life leads unto death, and death unto new life. Why, then, should death be taboo?",
+    "'THE NEW GENSHIN 1.4 UPDATE SUCKS', player says",
+    "whoever made this tweet is probably an idiot",
+    "Never gonna give you up, never gonna let you down",
+];
+
