20db5b23730f69787547bce1da62a7817cb30f89
Ein_Green <mbananola2002@gmail.com>
2021-03-20 00:02:25 -0700

    realized I forgot to comment, made comments, cried a bit

--- js/app.js
+++ js/app.js
@@ -7,11 +7,16 @@ var Tweets = [
     "This is a Tweet",
     "'Instagram is better', teen says",
     "Life leads unto death, and death unto new life. Why, then, should death be taboo?",
-    "'THE NEW GENSHIN 1.4 UPDATE SUCKS', player says",
+    "'THE NEW GENSHIN 1.4 UPDATE SUCKS', idiot player says",
     "whoever made this tweet is probably an idiot",
     "Never gonna give you up, never gonna let you down",
 ];
+// made a poor attempt to Rick-Roll again
 
+// this loop just iterates the tweets to the console
 for(var i = 0; i < 10; i ++) {
     console.log(Tweets[i]);
 }
+
+// tried the bonus, fail, tried again, failed again. 
+// kept trying, gave up
